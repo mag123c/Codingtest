f44d9b6e9e89b14bf5fb7ea171cabb71d5b1ee05
mag123c <120711308+mag123c@users.noreply.github.com>
2023-04-18 10:47:34 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,29 @@
+class Solution {
+	
+	static int join = 0;
+	
+    public int solution(int k, int[][] dungeons) {        
+        boolean[] bl = new boolean[dungeons.length];
+        
+        dfs(dungeons, bl, 0, k, 0, 0);
+        return join;
+    }
+    
+    public void dfs(int[][] dungeons, boolean[] bl, int sum, int k, int join2, int idx){
+    	if(idx==dungeons.length || sum > k) {
+    		join = Math.max(join, join2);
+    		return; 
+    	}    	
+    	
+    	for(int i=0; i<dungeons.length; i++) {
+    		if(bl[i]) continue;
+    		bl[i] = true;
+    		if(k-sum >= dungeons[i][0]) {
+    			dfs(dungeons, bl, sum+dungeons[i][1], k, join2+1, idx+1);
+    		}    		
+    		bl[i] = false;    		
+    	}
+        join = Math.max(join, join2);
+
+    }
+}
