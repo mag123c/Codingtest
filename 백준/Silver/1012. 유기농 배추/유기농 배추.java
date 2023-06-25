43afd78b5e9a10c3c18f535c50f5301ec85499e9
mag123c <120711308+mag123c@users.noreply.github.com>
2023-06-25 13:35:54 +0900

    Update 유기농 배추.java

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -29,7 +31,7 @@ public class Main {
             for(int j = 0; j < cabbage.length; j++){
                 for(int k = 0; k < cabbage[j].length; k++){
                     if(cabbage[j][k] == 1 && !bl[j][k]) {
-                        dfs(cabbage, bl, j, k);
+                        bfs(cabbage, bl, j, k);
                         result++;
                     }
                 }
@@ -39,15 +41,40 @@ public class Main {
         br.close();
     }
 
-    private static void dfs(int[][] cabbage, boolean[][] bl, int x, int y) {
-        if(x < 0 || x >= bl.length || y < 0 || y >= bl[0].length) return;
-        if(bl[x][y]) return;
+    private static void bfs(int[][] cabbage, boolean[][] bl, int x, int y) {
+        Queue<int []> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
         bl[x][y] = true;
-        if(cabbage[x][y] == 1){
-            dfs(cabbage, bl, x+1, y);
-            dfs(cabbage, bl, x-1, y);
-            dfs(cabbage, bl, x, y+1);
-            dfs(cabbage, bl, x, y-1);
+
+        int[] X = new int[]{-1, 1, 0, 0};
+        int[] Y = new int[]{0, 0, -1, 1};
+
+        while(!queue.isEmpty()){
+            int[] poll = queue.poll();
+
+            for(int i=0; i<4; i++){
+                int xx = poll[0] + X[i];
+                int yy = poll[1] + Y[i];
+
+                if(xx < 0 || xx >= bl.length || yy < 0 || yy >= bl[0].length) continue;
+
+                if(cabbage[xx][yy] == 1 && !bl[xx][yy]){
+                    queue.offer(new int[]{xx, yy});
+                    bl[xx][yy] = true;
+                }
+            }
         }
     }
+
+//    private static void dfs(int[][] cabbage, boolean[][] bl, int x, int y) {
+//        if(x < 0 || x >= bl.length || y < 0 || y >= bl[0].length) return;
+//        if(bl[x][y]) return;
+//        bl[x][y] = true;
+//        if(cabbage[x][y] == 1){
+//            dfs(cabbage, bl, x+1, y);
+//            dfs(cabbage, bl, x-1, y);
+//            dfs(cabbage, bl, x, y+1);
+//            dfs(cabbage, bl, x, y-1);
+//        }
+//    }
 }
