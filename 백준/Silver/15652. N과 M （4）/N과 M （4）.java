12cf6e6162798cf1f47ea537ead1af06e8cdc9b4
mag123c <120711308+mag123c@users.noreply.github.com>
2023-07-21 20:08:24 +0900

    [Silver III] Title: N과 M (4), Time: 180 ms, Memory: 22456 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int now, int idx) {
+        if(idx >= arr.length) {
+            for(int i : arr) sb.append(i + " ");
+
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = now; i <= N; i++) {
+            arr[idx] = i;
+            dfs(i, idx + 1);
+        }
+    }
+}
