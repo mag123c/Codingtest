039db34b0dde46eb4e999380ea8ddbdc81aac5c9
mag123c <120711308+mag123c@users.noreply.github.com>
2023-08-01 11:27:55 +0900

    [Silver III] Title: N과 M (8), Time: 192 ms, Memory: 18780 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] chk;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        chk = new int[M];
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        find(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void find(int idx, int cnt) {
+        if (cnt == M) {
+            StringBuilder tempSb = new StringBuilder();
+            for (int i = 0; i < chk.length; i++) {
+                tempSb.append(chk[i]).append(" ");
+            }
+            sb.append(tempSb).append("\n");
+
+            return;
+        }
+
+        for (int i = idx; i < N; i++) {
+            chk[cnt] = arr[i];
+            find(i, cnt + 1);
+        }
+    }
+}
