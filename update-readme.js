b266998b99459574a8c52868b7f5e4d7bbdacd9e
mag123c <diehreo@gmail.com>
2024-05-22 15:54:51 +0900

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -3,22 +3,26 @@ const execSync = require('child_process').execSync;
 
 const today = new Date().toISOString().slice(0, 10).replace(/-/g, '.');
 
+// Get the latest commit message without the " -BaekjoonHub" part
 const getCommitMessage = () => {
     const output = execSync('git log -1 --pretty=%B').toString().trim();
     return output.replace(' -BaekjoonHub', '');
 };
 
+// Get the list of files changed in the latest commit
 const getChangedFiles = () => {
     const output = execSync('git diff-tree --no-commit-id --name-only -r HEAD').toString().trim();
     return output.split('\n');
 };
 
+// Extract the problem link from the README file (any URL)
 const extractProblemLink = (readmeContent) => {
     const problemLinkRegex = /\[문제 링크\]\((https?:\/\/[^\s)]+)\)/;
     const match = readmeContent.match(problemLinkRegex);
     return match ? match[1] : null;
 };
 
+// Update the README file
 const updateReadme = (commitMessage, problemLink, readmeFilePath) => {
     let content = '';
 
