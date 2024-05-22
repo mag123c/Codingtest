16d1e970d0020db14ee8a720c423bc40c6abdb2b
mag123c <diehreo@gmail.com>
2024-05-22 20:35:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        System.out.println(word.charAt(0) + 0);
+    }
+}
