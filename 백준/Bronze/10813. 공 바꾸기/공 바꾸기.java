871902065c8b673ac0ec2d0a139a8724d76f0ec5
mag123c <diehreo@gmail.com>
2024-05-22 17:07:56 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] bucket = new int[N + 1];
+        for (int i = 0; i < bucket.length; i ++) {
+            bucket[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int tmp = bucket[start];
+            bucket[start] = bucket[end];
+            bucket[end] = tmp;
+        }
+
+        for(int i = 1; i < bucket.length; i ++) {
+            System.out.print(bucket[i] + " ");
+        }
+    }
+}
