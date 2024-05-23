e4b05f95fac33809cbe3b539c8aa164293e404ce
mag123c <diehreo@gmail.com>
2024-05-23 10:33:09 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
