0132e3c84dd6395db343f69f73cf7d74cb8d5fd1
mag123c <diehreo@gmail.com>
2024-05-24 16:05:19 +0900

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -40,6 +40,7 @@ const getDifficultyIconPath = (level) => {
     return `<div align="center"><img src="https://github.com/mag123c/Codingtest/blob/main/icon/${difficultyLevels[level] || 0}.svg" /></div>`;
 };
 
+//백준허브의 푸시인지 감지. 현재 LEETCODE 플랫폼은 이용중이지 않아서, 차후 추가해서 사용하면 됩니다.
 const getCommitMessages = () => {
     const output = execSync('git log -1 --pretty=%B').toString().trim();
     if (!output.includes('-BaekjoonHub')) {
@@ -60,7 +61,9 @@ const getCommitMessages = () => {
 
 const { problemLevel, problemTitle } = getCommitMessages();
 
-// 현재 커밋에 포함된 파일 경로와 파일명을 로그에 출력하는 함수
+//현재 커밋에 포함된 파일 경로와 파일명을 로그에 출력하는 함수
+//파일 경로를 받아오는 데는 성공하였으나, 한글과 특수문자의 디코딩이 되지 않아 푸시된 README를 인식하지 못하는 에러가 있습니다.
+//콘솔에 찍어보면 \312\443\531........../Title: \345\...... 형태의 README.md파일과 문제를 푼 언어의 코드파일이 감지는 되지만 디코딩이 되지 않는 이슈가 있습니다.
 const logUploadedFiles = () => {
     try {
         const output = execSync('git diff-tree --no-commit-id --name-only -r HEAD').toString().trim();
@@ -85,6 +88,11 @@ const decodeFilePath = (filePath) => {
 
 const uploadedFiles = logUploadedFiles().map(decodeFilePath);
 
+//위의 logUploadedFiles에서 경로를 받아오면 베스트이지만
+//현재 이슈 해결을 하지 못해 임시로 solved.ac API를 사용해서, 문제 제목으로 조회하여 문제 번호를 가져와서 링크에 사용하고 있습니다.
+//같은 문제 제목일 경우, 맨 앞의 문제를 가져오기 때문에 정확하지 않을 수 있습니다.
+//크롤링을 통해 https://www.acmicpc.net/search#q=${문제이름}&c=Problems 으로 사용하려고 하였으나
+//403이 발생하였고 확인 결과 현재 스타트링크에서는, 스크래핑은 차단하고 있다고 합니다.
 const fetchProblemLink = async (title) => {
     try {
         const reqUrl = `https://solved.ac/api/v3/search/problem?query=${title}&page=1`;
