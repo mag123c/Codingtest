a5d1ceec927bff6e9e6b6ee458ff991ccc206814
mag123c <diehreo@gmail.com>
2024-05-29 09:48:53 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14396 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,26 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 
-import static java.math.BigInteger.*;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger[] arr = new BigInteger[51];
-
-        for (int i = 0; i <= 50; i ++) {
-            arr[i] = valueOf((long) Math.pow(31, i));
-        }
-
         int N = Integer.parseInt(br.readLine());
         String s = br.readLine();
         char[] c = s.toCharArray();
-        BigInteger answer = valueOf(0);
+
+        BigInteger hash = BigInteger.valueOf(0);
         for (int i = 0; i < N; i ++) {
-            BigInteger tmp = arr[i].multiply(valueOf(c[i] - 'a' + 1));
-            answer = answer.add(tmp);
+            hash = hash.add(BigInteger.valueOf(c[i] - 96).multiply(BigInteger.valueOf(31).pow(i)));
         }
-        System.out.println(answer);
+
+        System.out.println(hash.remainder(BigInteger.valueOf(1234567891)));
     }
 }
