586cc49864a809f7f87f728477d03856e5f49a8c
JaeHo Jang <diehreo@gmail.com>
2024-06-30 13:39:54 +0900

    [Silver IV] Title: 스케이트 연습, Time: 384 ms, Memory: 68364 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28324. 스케이트 연습/스케이트 연습.java
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.java
@@ -4,35 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] speed = new int[N];
-        int[] dp = new int[N];
-
+        
         for (int i = 0; i < N; i++) {
             speed[i] = Integer.parseInt(st.nextToken());
         }
 
-        int cur = 0;
+        long cur = 0;
+        long maxSum = 0;
         for (int i = N - 1; i >= 0; i--) {
             if (cur + 1 > speed[i]) {
                 cur = speed[i];
             } else {
                 cur++;
             }
-            dp[i] = cur;
+            maxSum += cur;
         }
 
-        int max = 0;
-        for (int i : dp) {
-            max += i;
-        }
-
-        System.out.println(max);
-
+        System.out.println(maxSum);
     }
 }
