0c92a679c9b17482eb0f4816fa82af8aae39c3e7
JaeHo Jang <diehreo@gmail.com>
2024-07-14 20:28:07 +0900

    [Gold IV] Title: 사이클 게임, Time: 628 ms, Memory: 125104 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (find(parent, a) != find(parent, b)) {
+                union(parent, a, b);
+                cnt++;
+            }            
+            else {
+                System.out.println(++cnt);
+                return;
+            }
+        }
+
+        System.out.println(0);        
+    }
+
+    private static int find(int[] parent, int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent, parent[x]);
+    }
+
+    private static void union(int[] parent, int a, int b) {
+        a = find(parent, a);
+        b = find(parent, b);
+
+        if (a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+}
