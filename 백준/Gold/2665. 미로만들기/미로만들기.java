489a3901b91dcb2bc60109340cc3912ea120ec53
JaeHo Jang <diehreo@gmail.com>
2024-09-18 15:28:21 +0900

    [Gold IV] Title: 미로만들기, Time: 128 ms, Memory: 16412 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,66 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    
+    static int n;
+    static int[][] board;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] board = new int[N][N];
+        n = Integer.parseInt(br.readLine().trim());
+        board = new int[n][n];
+        dist = new int[n][n];
 
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < N; j++) {
-                board[i][j] = line.charAt(j) - '0';
-            }
-        }
-
-        System.out.println(bfs(board, N));
-    }
-
-    private static int bfs(int[][] board, int N) {
-        Deque<int[]> q = new LinkedList<>();
-        int[][] dist = new int[N][N];
-        int[] dx = new int[] {-1, 1, 0, 0};
-        int[] dy = new int[] {0, 0, -1, 1};
-
-        for (int i = 0; i < N; i ++) {
-            for (int j = 0; j < N; j ++) {
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine().trim();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = temp.charAt(j) - '0';
                 dist[i][j] = Integer.MAX_VALUE;
             }
         }
 
-        q.offer(new int[] {0, 0});
-        dist[0][0] = 0;
+        bfs();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(dist[n - 1][n - 1] + "\n");
+        bw.flush();
+        bw.close();
+    }
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int curX = cur[0];
-            int curY = cur[1];
+private static void bfs() {
+    Queue<Node> q = new LinkedList<>();
+    q.offer(new Node(0, 0));
+    dist[0][0] = 0;
 
-            for (int i = 0; i < 4; i ++) {
-                int nX = curX + dx[i];
-                int nY = curY + dy[i];
+    while (!q.isEmpty()) {
+        Node current = q.poll();
 
-                if (nX >= 0 && nX < N && nY >= 0 && nY < N) {
-                    int newDist = dist[curX][curY];
-                    if (newDist < dist[nX][nY]) {
-                        if (board[nX][nY] == 0) {
-                            q.offerLast(new int[] {nX, nY});
-                            dist[nX][nY] = newDist + 1;
-                        }
-                        else {
-                            q.offerFirst(new int[] {nX, nY});
-                            dist[nX][nY] = newDist;
-                        }
-                    }
+        for (int i = 0; i < 4; i++) {
+            int nx = current.x + dx[i];
+            int ny = current.y + dy[i];
+
+            if (isValid(nx, ny)) {
+                int nextDist = dist[current.x][current.y] + (board[nx][ny] == 0 ? 1 : 0);
+                if (dist[nx][ny] > nextDist) {
+                    dist[nx][ny] = nextDist;
+                    q.offer(new Node(nx, ny));
                 }
             }
         }
+    }
+}
 
-        return dist[N - 1][N - 1];
+
+    private static boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x < n && y < n;
+    }
+
+    private static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
