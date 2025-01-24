660311a728033a6893ace7f52b652b33b8a23741
JaeHo Jang <diehreo@gmail.com>
2025-01-24 15:38:10 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -4,29 +4,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-class Main{
-	
-    public static void main(String[] args) throws IOException{    	
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	
-    	int max = 0;
-    	int idx = 0;
-    	for(int i=1; i<=9; i++) {
-    		int number = Integer.parseInt(br.readLine());    		
-            
-    		if(max < number) {
-    			max = number;
-    			idx = i;
-    		}
-    	}
-    	
-    	bw.write(Integer.toString(max));
-    	bw.newLine();
-    	bw.write(Integer.toString(idx));
-    	
-    	bw.close();    	
-    
-    }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int max = 0;
+        int maxIndex = 0;
+        for (int i = 1; i <= 9; i ++) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (number > max) {
+                max = number;
+                maxIndex = i;
+            }
+        }
+
+        bw.write(max + "\n" + maxIndex);
+        bw.flush();
+        bw.close();
+    }
 }
