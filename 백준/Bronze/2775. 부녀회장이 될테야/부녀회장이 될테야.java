8ce588de03c437861706242541d91d9ec129481f
JaeHo Jang <diehreo@gmail.com>
2025-07-18 15:13:16 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i ++) {
+        for (int i = 0; i < N; i ++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
-            int[][] apt = new int[k + 1][n + 1];
 
-            dp(k, n, apt);
-            System.out.println(apt[k][n]);        }
-    }
+            int[][] dp = new int[k + 1][n + 1];
 
-    private static void dp(int k, int n, int[][] apt) {
-        for (int i = 1; i <= n; i ++) {
-            apt[0][i] = i;
-        }
-
-        for (int i = 1; i <= k; i ++) {
             for (int j = 1; j <= n; j ++) {
-                apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
+                dp[0][j] = j;
             }
+
+            for (int f = 1; f <= k; f ++) {
+                for (int r = 1; r <= n; r ++) {
+                    dp[f][r] = dp[f][r - 1] + dp[f - 1][r];
+                }
+            }
+
+            System.out.println(dp[k][n]);
         }
     }
 }
